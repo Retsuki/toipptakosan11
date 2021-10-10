2bffbc841f6bf8962ef26fb358aa50994ba2758f
Retsuki <s1f101701845@iniad.org>
2021-10-10 13:25:44 +0900

    ADD: member detail page

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './pages/Home.vue'
 import Member from './pages/Member.vue'
+import MemberDetail from './pages/MemberDetail.vue'
 
 
 const routes = [
@@ -13,6 +14,10 @@ const routes = [
     path: '/member',
     name: 'Member',
     component: Member
+  },
+  {
+    path: '/member/:id',
+    component: MemberDetail
   }
 ]
 
