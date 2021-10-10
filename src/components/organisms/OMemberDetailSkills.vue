b323955c294b901c762810b1657f03416577931e
Retsuki <s1f101701845@iniad.org>
2021-10-10 19:24:15 +0900

    ADD: skills

---
+++ src/components/organisms/OMemberDetailSkills.vue
@@ -0,0 +1,56 @@
+<template>
+  <div :class="$style.wrapper">
+    <div>Skills</div>
+
+    <ul :class="$style.tags">
+      <li v-for="skill of skills" :key="skill">
+        <span :class="$style.tag">{{ skill }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    skills: {
+      type: Array,
+      required: true
+    }
+  },
+  setup () {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" module>
+.wrapper {
+  padding-top: 60px;
+
+  & > div {
+    font-size: 2.4rem;
+    font-weight: 700;
+  }
+}
+
+.tags {
+  display: flex;
+  flex-wrap: wrap;
+
+  & > li {
+    padding: 10px 8px;
+  }
+}
+
+.tag {
+  display: inline-block;
+  padding: 6px 16px;
+  color: #fff;
+  background-color: #666666;
+  border-radius: 25px;
+  font-weight: 700;
+}
+</style>
