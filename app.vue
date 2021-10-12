1cf08876b9870be1a0ecf1e0615d74592e9a422a
Retsuki <s1f101701845@iniad.org>
2021-10-13 04:59:12 +0900

    UPDATE: APP.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <!-- Remove this component to get started! -->
-    <NuxtWelcome />
+    <o-global-header />
+    <NuxtPage :class="$style.mainContents" />
+    <o-global-footer />
   </div>
 </template>
+
+<style lang="scss" module>
+.mainContents {
+  min-height: 100vh;
+  max-height: 100%;
+}
+</style>
