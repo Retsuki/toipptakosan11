b722daa43165dc017919cd9ec659df1540792459
Retsuki <s1f101701845@iniad.org>
2021-10-14 09:54:17 +0900

    DELETE: functions

--- functions/lib/members/members.service.d.ts
+++
@@ -1,5 +0,0 @@
-import { Member } from 'src/members/interfaces/member.interface';
-export declare class MembersService {
-    getMember(id: string): Promise<Member>;
-    getMembers(): Promise<Member[]>;
-}
