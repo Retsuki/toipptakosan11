7f7d46ee0026ab56b6ce7b1e0ee9b2ab11aa7424
Retsuki <s1f101701845@iniad.org>
2021-10-30 23:11:28 +0900

    add: mauth

---
+++ frontend/src/components/molecules/MAuth.vue
@@ -0,0 +1,58 @@
+<template>
+  <div :class="$style.wrapper">
+    <a-input
+      type="text"
+      placeholder="Email"
+      :class="$style.content"
+      :value="email"
+      @input="$emit('update:email', $event.target.value)"
+    />
+
+    <a-input
+      type="password"
+      placeholder="Password"
+      :class="$style.content"
+      :value="password"
+      @input="$emit('update:password', $event.target.value)"
+    />
+
+    <a-button
+      :text="buttonText"
+      :onClick="onClick"
+      :class="$style.button"
+    ></a-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    email: String,
+    password: String,
+    buttonText: String,
+    onClick: Function,
+  },
+})
+</script>
+
+<style lang="scss" module>
+.wrapper {
+  width: 250px;
+  padding: 30px 0;
+  display: flex;
+  flex-direction: column;
+}
+
+.content {
+  height: 28px;
+  margin-bottom: 15px;
+}
+
+.button {
+  padding: 16px;
+  color: $color-white;
+  background-color: $color-black;
+}
+</style>
