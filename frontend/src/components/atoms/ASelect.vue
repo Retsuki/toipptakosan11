d3861d11106cba18a18f92bad1f3ec8fe7164064
Retsuki <s1f101701845@iniad.org>
2021-11-03 01:07:40 +0900

    add: aselect and mlabelselect

---
+++ frontend/src/components/atoms/ASelect.vue
@@ -0,0 +1,42 @@
+<template>
+  <select
+    :name="name"
+    :class="$style.select"
+    :value="modelValue"
+    @change="$emit('update:modelValue', $event.currentTarget)"
+  >
+    <option value="">--選択してください--</option>
+    <slot />
+  </select>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      default: '',
+      required: false,
+    },
+    modelValue: {
+      type: String,
+      default: '',
+      required: false,
+    },
+  },
+  emits: ['update:modelValue'],
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style lang="scss" module>
+.select {
+  border: $border;
+  border-radius: $radius;
+  padding: 10px;
+}
+</style>
