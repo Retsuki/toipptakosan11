c2a4e6f95891c7072c94b078cef6f725705577f0
Retsuki <s1f101701845@iniad.org>
2021-11-03 13:05:22 +0900

    add: textarea component

---
+++ frontend/src/components/atoms/ATextarea.vue
@@ -0,0 +1,50 @@
+<template>
+  <textarea
+    :name="name"
+    :cols="cols"
+    :rows="rows"
+    :placeholder="placeholder"
+    :class="$style.textarea"
+  ></textarea>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      default: '',
+      required: false,
+    },
+    rows: {
+      type: Number,
+      default: 10,
+    },
+    cols: {
+      type: Number,
+      default: 30,
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style lang="scss" module>
+.textarea {
+  border: $border;
+  border-radius: $radius;
+  padding: 10px;
+}
+</style>
