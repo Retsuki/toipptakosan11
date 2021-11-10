3f014e056d8eef0aa497ec2d8c903b12ab60f342
Retsuki <s1f101701845@iniad.org>
2021-11-10 09:53:36 +0900

    add: img atoms

---
+++ frontend/src/components/atoms/AImg.vue
@@ -0,0 +1,48 @@
+<template>
+  <img
+    :src="src"
+    :width="width"
+    :height="height"
+    :alt="alt"
+    :class="[
+      {
+        [$style.circle]: circle,
+      },
+    ]"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    src: {
+      type: String,
+      require: true,
+    },
+    width: {
+      type: String,
+      default: '150',
+    },
+    height: {
+      type: String,
+      default: '150',
+    },
+    alt: {
+      type: String,
+      default: 'image',
+    },
+    circle: {
+      type: Boolean,
+      default: false,
+    },
+  },
+})
+</script>
+
+<style lang="scss" module>
+.circle {
+  border-radius: $circle;
+}
+</style>
