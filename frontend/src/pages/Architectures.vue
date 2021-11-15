b385f10392d953366878024ec03c44c54eb93048
Retsuki <s1f101701845@iniad.org>
2021-11-15 09:46:00 +0900

    update: pages architecures component

--- frontend/src/pages/Architectures.vue
+++ frontend/src/pages/Architectures.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>archtectures</div>
+  <t-architectures :architectures="architectures" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import TArchitectures from '../components/templates/TArchitectures.vue'
 
 export default defineComponent({
+  components: { TArchitectures },
   setup() {
-    return {}
+    const architectures = ref([
+      {
+        id: '1',
+        img: 'src/assets/images/default.png',
+        name: '銀行連携アーキテクチャ',
+        tags: ['freee', 'GCP', 'Redis'],
+      },
+      {
+        id: '2',
+        img: 'src/assets/images/default.png',
+        name: '銀行連携アーキテクチャ',
+        tags: ['freee', 'GCP', 'Redis'],
+      },
+      {
+        id: '3',
+        img: 'src/assets/images/default.png',
+        name: '銀行連携アーキテクチャ',
+        tags: ['freee', 'GCP', 'Redis'],
+      },
+      {
+        id: '4',
+        img: 'src/assets/images/default.png',
+        name: '銀行連携アーキテクチャ',
+        tags: ['freee', 'GCP', 'Redis'],
+      },
+    ])
+    return { architectures }
   },
 })
 </script>
