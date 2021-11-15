508c4d4d460d8ef204c236932e30e12c2ca272fd
Retsuki <s1f101701845@iniad.org>
2021-11-15 09:45:46 +0900

    add: templates architectures component

---
+++ frontend/src/components/templates/TArchitectures.vue
@@ -0,0 +1,52 @@
+<template>
+  <div :class="$style.contents">
+    <o-architectures
+      v-for="{ img, name, tags } of architectures"
+      :key="img"
+      :img="img"
+      :name="name"
+      :tags="tags"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+interface Card {
+  img: string
+  name: string
+  tags: string[]
+}
+
+export default defineComponent({
+  props: {
+    architectures: {
+      type: Array as PropType<Card[]>,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" module>
+.contents {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  justify-content: center;
+  padding-top: 60px;
+  padding-bottom: 120px;
+  row-gap: 30px;
+
+  @media (min-width: 760px) {
+    grid-template-columns: 375px 375px;
+    column-gap: 15px;
+  }
+
+  @media (min-width: 1125px) {
+    grid-template-columns: 375px 375px 375px;
+    column-gap: 15px;
+  }
+}
+</style>
