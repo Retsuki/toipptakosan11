a453cb96c4989ed9df93f22bf87adb5a8d22d3a3
Retsuki <s1f101701845@iniad.org>
2021-11-15 09:44:13 +0900

    add: ATag and MTagGroup components

---
+++ frontend/src/components/atoms/ATag.vue
@@ -0,0 +1,51 @@
+<template>
+  <span :class="[$style.tag, $style[size]]" :style="styleComputed">
+    {{ text }}
+  </span>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    textColor: {
+      type: String,
+      default: '#000',
+    },
+    color: {
+      type: String,
+      default: '#e0e0e0',
+    },
+    size: {
+      type: String,
+      default: 'm',
+    },
+  },
+  setup(props) {
+    const styleComputed = computed(() => {
+      return {
+        color: props.textColor,
+        backgroundColor: props.color,
+      }
+    })
+    return { styleComputed }
+  },
+})
+</script>
+
+<style lang="scss" module>
+.tag {
+  border-radius: 25px;
+  padding-left: 16px;
+  padding-right: 16px;
+  align-items: center;
+  display: inline-flex;
+  font-size: $font-size-m;
+  height: 32px;
+}
+</style>
---
+++ frontend/src/components/molecules/MTagGroup.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :class="$style.tagGroup">
+    <a-tag
+      v-for="tag of tags"
+      :key="tag"
+      :text="tag"
+      :size="tagSize"
+      :class="$style.tag"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  props: {
+    tags: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
+    tagSize: {
+      type: String,
+      default: 'l',
+    },
+  },
+})
+</script>
+
+<style lang="scss" module>
+.tagGroup {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.tag {
+  margin: 0 8px 0 0;
+}
+</style>
